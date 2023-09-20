f73ddc531b4fc8f813e94aff165d805c9a1a0b60
Tobias Fellinger <tobias.fellinger@ages.at>
2023-09-20 13:18:15 +0200

    cleaned up doku and description

--- R/internals.R
+++ R/internals.R
@@ -1,12 +1,12 @@
-#' Check t and lambda
-#'
-#' @param t time interval starting points
-#' @param lambda hazards
-#'
-#' @details
-#' check if t and lambda have valid and compatible size, sign and order
-#'
-#' @return TRUE invisibly
+# Check t and lambda
+#
+# @param t time interval starting points
+# @param lambda hazards
+#
+# @details
+# check if t and lambda have valid and compatible size, sign and order
+#
+# @return TRUE invisibly
 check_t_lambda <- function(t, lambda){
   checkmate::assert_numeric(t, sorted = TRUE, unique=TRUE)
   checkmate::assert_numeric(lambda, lower=0)
