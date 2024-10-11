78a7126ceec061cc59459125de3d5f9faadeaa77
Fellinger Tobias <tobias.fellinger@ages.at>
2024-10-11 11:54:09 +0200

    continued with plotting method

--- R/s3methods.R
+++ R/s3methods.R
@@ -4,12 +4,44 @@
 #' @param what what to plot ("d", "p", "q", "h", "ch", "s")
 #' @param from lower x-Axis limit
 #' @param to upper x-Axis limit
-#' @param ... passed on to base::curve
+#' @param mfrow plot layout defaults to all plots in one row
+#' @param n number of points for interpolation
+#' @param ... passed on to base::plot
+#'
+#' @details
+#'
+#' @export
+#'
+#' The layout uses the mfrow argument to par and defaults to all plots in one
+#' row. The layout can be overwritten by passing the mfrow argument, that is
+#' passed as is to an internal call to par.
 #'
 #' @return
+#' NULL, invisibly
 #'
 #' @examples
-plot.miniPCH <- function(obj, what=c("d", "s", "h"), from, to, ...){
+#' my_pch <- pch_functions(c(0, 3), c(2, 0.1))
+#' plot(my_pch)
+#'
+#' Tint <- c(0,3)
+#' Q <- array(
+#'   c(
+#'     -0.3,  0  , 0,
+#'     0.2, -0.4, 0,
+#'     0.1,  0.4, 0,
+#'
+#'     -0.3,  0  , 0,
+#'     0.2, -0.2, 0,
+#'     0.1,  0.2, 0
+#'   ), dim=c(3,3,2)
+#' )
+#'
+#' pi <- c(1,0,0)
+#' abs <- c(0,0,1)
+#'
+#' my_obj <- multistate_functions(Tint, Q, pi, abs)
+#' plot(my_obj)
+plot.miniPCH <- function(obj, what=c("d", "s", "h"), from, to, mfrow=c(1,length(what)), n=1001, ...){
   if(!all(what %in% c("d", "p", "q", "h", "ch", "s"))){
     stop(gettext("Argument what has to be a character vector containing only d, p, q, h, ch, s, to plot density, cdf, quantiles, hazard, cumulative hazard or survival function"))
   }
@@ -34,10 +66,36 @@ plot.miniPCH <- function(obj, what=c("d", "s", "h"), from, to, ...){
     to <- max(q, t_max)
   }
 
-  par(mfrow=c(1,length(what)))
+  par(mfrow=mfrow)
+
+  x <- seq(from, to, length.out=n)
   for(i in what){
-    curve(obj[[i]](x), from=from, to=to, xlab="t", ylab=i, ...)
+    if(i %in% c("d", "h")){
+      plot_cadlag(x, obj[[i]], obj$t, xlab="t", ylab=i, ...)
+    } else {
+      plot(x, obj[[i]](x), xlab="t", ylab=i,  type="l", ...)
+    }
     abline(v=obj$t, col="grey")
   }
+
+  invisible(NULL)
 }
 
+
+plot_cadlag <- function(x, fun, jumps, ...){
+  jumps <- c(jumps, Inf) |>
+    sort() |>
+    unique()
+
+  plot(x, fun(x), type="n", ...)
+  Map(\(i){
+    tmp_x <- c(jumps[i], x[(x > jumps[i]) & (x < jumps[i+1])])
+    lines(tmp_x, fun(tmp_x))
+    points(jumps[i], fun(jumps[i]), pch=18)
+    if(i != 1L){
+      points(jumps[i], fun(jumps[i]-2*.Machine$double.eps), pch=1)
+    }
+  }, 1:(length(jumps)-1))
+
+  invisible(NULL)
+}
